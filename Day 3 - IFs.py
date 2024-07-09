e71375bdda280af2f0dc14dd699a60faaf9f32c4
AVale97 <Vale50@outlook.pt>
2024-07-09 18:05:08 +0100

    Create Day 3 - IFs.py

---
+++ Day 3 - IFs.py
@@ -0,0 +1,47 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+
+choice1 = input('You come to a fork in the road, you have two options left or right? Type "left" or "right".')
+if choice1 == "left":
+          print ("The stars are with you, you progress to a large field where you see a large lake")
+else:
+          print ("You dead as hell")
+choice2 = input ("You look over to the other side of the lake, do you swim? Type Y or N")
+
+if choice2 == "N":
+          print ("You wait for a boat, a sailor takes you to the other side")
+else:
+          print ("You dead as hell")
+
+choice3 = input('The sailor takes you to a pyramid where he says you will be tested, you see 3 doors in front of you. One is red, the other blue and the last one is yellow, which one do you open? "Red", Yellow" or "Blue"')
+
+if choice3 == "Yellow":
+          print ("You found the treasure of 1.000.000.000.000$, you are rich now")
+else:
+          print ("You dead as hell")
